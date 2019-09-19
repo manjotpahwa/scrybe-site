56537e982f148b3d0d8e67675cbe5717fdada960
Manjot <manjotpahwa@gmail.com>
2019-09-19 14:50:29 +0530

    Trim down the send_contact page to bare minimum

--- send_mail_contact.php
+++ send_mail_contact.php
@@ -6,8 +6,6 @@ This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
 $contact_page = "contact.html";
-$error_page = "error_message.html";
-$thankyou_page = "thank_you.html";
 
 /*
 This next bit loads the form field data into variables.
@@ -18,10 +16,6 @@ $email_address = $_REQUEST['email'] ;
 // If the user tries to access this script directly, redirect them to the contact form,
 if (!isset($_REQUEST['email'])) {
 header( "Location: $contact_page" );
-}
-
-elseif empty($email_address) {
-header( "Location: $error_page" );
 }
 
 // If we passed all previous tests, send the email then redirect to the thank you page.
