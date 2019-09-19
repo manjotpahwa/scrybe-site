2e8abf3396da7253eabafedff981e218b1b94a1c
Manjot <manjotpahwa@gmail.com>
2019-09-19 15:24:31 +0530

    Adding debug echo messages.

--- send_mail_contact.php
+++ send_mail_contact.php
@@ -7,11 +7,19 @@ If you change the names of any of the pages, you will need to change the values 
 */
 $contact_page = "contact.html";
 
+echo 'We\'ve reached here.'
+
 /*
 This next bit loads the form field data into variables.
 If you add a form field, you will need to add it here.
 */
 $email_address = $_POST['email'] ;
+
+echo 'Email address from post request' . $email_address;
+
+$email_address = $_REQUEST['email'] ;
+
+echo 'Email address from only request' . $email_address;
 
 // If the user tries to access this script directly, redirect them to the contact form,
 if (!isset($_POST['email'])) {
